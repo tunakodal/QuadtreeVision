35d0c21ce01bda33faa4f1bb41f7ca1408426706
Tuna Kodal <tkodal@etu.edu.tr>
2025-12-05 21:20:33 +0300

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,106 @@
+import java.io.IOException;
+
 public class Main {
-    static double[] compression_levels = {0.002, 0.004, 0.01, 0.033, 0.077, 0.2, 0.5, 0.65};
-    static double[] tolerance_levels = {0.0005, 0.0007, 0.0015, 0.002, 0.01, 0.015, 0.02, 0.03};
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
+        boolean compression_flag = false;
+        boolean edge_flag = false;
+        boolean outline_flag = false;
+        String input_file = null;
+        String output_file = null;
+
+       double[] compression_levels = {0.002, 0.004, 0.01, 0.033, 0.077, 0.2, 0.5, 0.65};
+       double[] tolerance_levels = {0.0005, 0.0007, 0.0015, 0.002, 0.007, 0.015, 0.02, 0.03};
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-c")) {
+                compression_flag = true;
+            } else if (args[i].equals("-e")) {
+                edge_flag = true;
+            } else if (args[i].equals("-t")) {
+                outline_flag = true;
+            }
+
+            if (args[i].equals("-i"))
+                input_file = args[i+1];
+
+            if (args[i].equals("-o"))
+                output_file = args[i+1];
+        }
+
+        if (input_file == null || output_file == null) {
+            System.out.println("Input or output file not specified. Invalıd arguments.");
+            return;
+        }
+
+        if (!compression_flag && !edge_flag) {
+            System.out.println("Either -c or -e must specified.");
+            return;
+        }
+
+        PPMImage image = PPMAnalyzer.read_PPM(input_file);
+        int width  = image.getWidth();
+        int height = image.getHeight();
+
+        // Only square images are allowed
+        if (width != height) {
+            System.out.println("Error: Only square images are supported in this assignment.");
+            return;
+        }
+
+        if (compression_flag){
+            for (int i = 0; i < compression_levels.length; i++) {
+                double threshold = findThreshold(image,
+                                    compression_levels[i],
+                                    tolerance_levels[i],
+                                    image.getMaxColorValue(),
+                          image.getWidth()* image.getHeight());
+
+                if (threshold == -2){
+                    System.out.println("Image " + (i + 1) + ":");
+                    System.out.println(" -No suitable threshold found for the value: " + compression_levels[i]);
+                    continue;
+                }
+
+                QuadTree qt = new QuadTree(image, threshold);
+                PPMImage outImage = qt.render(outline_flag);
+
+                int leafCount = qt.countLeaves();
+                double achievedLevel = (double) leafCount / (double) (width * height);
+                String achievedLevelString = String.valueOf(achievedLevel);
+                achievedLevelString = String.format("%.5f", achievedLevel);
+
+
+
+                System.out.println("Image " + (i + 1) + ":");
+                System.out.println(" -Target compression level : " + compression_levels[i]);
+                System.out.println(" -Leaf count               : " + leafCount);
+                System.out.println(" -Pixel count               : " + width*height);
+                System.out.println(" -Achieved compression lvl : " + achievedLevelString);
+                System.out.println();
+
+                String output_file_name = output_file.substring(0,output_file.indexOf("."));
+                String output_name = output_file_name + "-" + (i+1) + ".ppm";
+
+                PPMAnalyzer.write_PPM(output_name, outImage);
+            }
+        }
+
+        if (edge_flag){
+            int defaultThreshold = 30;
+
+            QuadTree qt = new QuadTree(image, defaultThreshold);
+            PPMImage outImage = qt.edgeDetect(outline_flag);
+
+            String output_file_name = output_file.substring(0,output_file.indexOf("."));
+            String output_name = output_file_name + ".ppm";
+
+            PPMAnalyzer.write_PPM(output_name, outImage);
+        }
     }
 
-    public static Double findThreshold(PPMImage image,
+    public static double findThreshold(PPMImage image,
                                        double compressionLevel,
                                        double toleranceLevel,
                                        int MaxColorValue,
@@ -37,7 +131,7 @@ public class Main {
             }
         }
 
-        return null; // Suitable threshold is not found
+        return -2; // Suitable threshold is not found
     }
 
 
